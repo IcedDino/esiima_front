f7bc48b8a5c78ed4645712e838cb5ced2f62bd2c
IcedDino <nullpointer@floresr.com>
2025-11-22 14:35:48 -0600

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'main.html'),
+        index: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+});
